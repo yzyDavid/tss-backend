02a560f72e142aca8ce4da02bda39016349ac2a3
Zhenyun Yu <yzyDavid@qq.com>
2018-03-15 23:26:02 +0800

    init app.

---
+++ src/main/java/tss/TssApplication.java
@@ -0,0 +1,14 @@
+package tss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yzy
+ */
+@SpringBootApplication
+public class TssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TssApplication.class, args);
+    }
+}
