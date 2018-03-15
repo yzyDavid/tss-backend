7bf1c0e86c54f8f5984ad1ea0f52f7d11925a2dd
Zhenyun Yu <yzyDavid@qq.com>
2018-03-16 01:50:46 +0800

    refactor.

---
+++ src/main/java/tss/configs/Config.java
@@ -0,0 +1,8 @@
+package tss.configs;
+
+/**
+ * @author yzy
+ */
+public class Config {
+    public static final String FRONTEND_URL = "";
+}
--- src/main/java/tss/echo/EchoController.java
+++ src/main/java/tss/echo/EchoController.java
@@ -1,4 +1,4 @@
-package tss;
+package tss.echo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
