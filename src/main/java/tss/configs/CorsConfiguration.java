9c32fb5639c967a332aa00fb555ef179a0d835d1
Zhenyun Yu <yzyDavid@qq.com>
2018-03-16 01:57:32 +0800

    enable cors.

---
+++ src/main/java/tss/configs/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package tss.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author yzy
+ */
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
