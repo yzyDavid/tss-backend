052c2706693f13c0167cb85f31068f4d4b9b2848
Zhenyun Yu <yzyDavid@qq.com>
2018-03-17 12:16:37 +0800

    added secure utils.

---
+++ src/main/java/tss/information/SecurityUtils.java
@@ -0,0 +1,38 @@
+package tss.information;
+
+import org.jetbrains.annotations.*;
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * @author yzy
+ */
+public class SecurityUtils {
+    public static @NotNull
+    @NonNls
+    String getSalt() {
+        Random rand = new SecureRandom();
+        byte[] salt = new byte[16];
+        rand.nextBytes(salt);
+        return new BASE64Encoder().encode(salt);
+    }
+
+    public static @Nullable String getHashedPasswordByPasswordAndSalt(@NotNull String password, @NotNull @NonNls String salt) {
+        String plain = salt + password;
+        MessageDigest messageDigest;
+        String cipher = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(plain.getBytes("UTF-8"));
+            cipher = new BASE64Encoder().encode(messageDigest.digest());
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return cipher;
+    }
+}
