592c2e7dfff65a324f3bb695df5acd01f564ac38
Zhenyun Yu <yzyDavid@qq.com>
2018-03-17 11:34:05 +0800

    added annotations.

---
+++ src/main/java/tss/session/AuthorizationInterceptor.java
@@ -0,0 +1,11 @@
+package tss.session;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author yzy
+ */
+@Component
+public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
+}
