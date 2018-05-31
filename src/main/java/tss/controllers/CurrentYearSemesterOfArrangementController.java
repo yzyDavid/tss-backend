feae75aab42f91142fe2473e9a6e74cbacdf7419
reeve <ruifeng_chen@foxmail.com>
2018-05-31 11:45:35 +0800

    refactor CurrentYearSemesterOfArrangementController

--- src/main/java/tss/controllers/CurrentYearSemesterOfArrangementController.java
+++ src/main/java/tss/controllers/CurrentYearSemesterOfArrangementController.java
@@ -2,10 +2,15 @@ package tss.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import tss.entities.ClassroomEntity;
 import tss.entities.CurrentYearSemesterOfArrangementEntity;
+import tss.entities.TimeSlotEntity;
 import tss.exceptions.CurrentYearSemesterOfArrangementNotFoundException;
 import tss.models.YearSemester;
+import tss.repositories.ClassRepository;
+import tss.repositories.ClassroomRepository;
 import tss.repositories.CurrentYearSemesterOfArrangementRepository;
+import tss.repositories.TimeSlotRepository;
 
 import java.util.List;
 
@@ -17,14 +22,22 @@ import java.util.List;
 public class CurrentYearSemesterOfArrangementController {
 
     private CurrentYearSemesterOfArrangementRepository currentYearSemesterOfArrangementRepository;
+    private ClassroomRepository classroomRepository;
+    private TimeSlotRepository timeSlotRepository;
 
-    public CurrentYearSemesterOfArrangementController(CurrentYearSemesterOfArrangementRepository currentYearSemesterOfArrangementRepository) {
+    public CurrentYearSemesterOfArrangementController(
+            CurrentYearSemesterOfArrangementRepository currentYearSemesterOfArrangementRepository,
+            ClassroomRepository classroomRepository, TimeSlotRepository timeSlotRepository) {
         this.currentYearSemesterOfArrangementRepository = currentYearSemesterOfArrangementRepository;
+        this.classroomRepository = classroomRepository;
+        this.timeSlotRepository = timeSlotRepository;
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void setCurrentYearSemesterOfArrangement(@RequestBody YearSemester yearSemester) {
+
+        // Invalidate the old one.
         List<CurrentYearSemesterOfArrangementEntity> validYearSemesters = currentYearSemesterOfArrangementRepository
                 .findByValid(true);
         for (CurrentYearSemesterOfArrangementEntity validYearSemester : validYearSemesters) {
@@ -32,6 +45,15 @@ public class CurrentYearSemesterOfArrangementController {
             currentYearSemesterOfArrangementRepository.save(validYearSemester);
         }
 
+        // Clear all existing arrangements of classes.
+        for (ClassroomEntity classroomEntity : classroomRepository.findAll()) {
+            for (TimeSlotEntity timeSlotEntity : classroomEntity.getTimeSlots()) {
+                timeSlotEntity.setClazz(null);
+                timeSlotRepository.save(timeSlotEntity);
+            }
+        }
+
+        // Persist the new one.
         CurrentYearSemesterOfArrangementEntity newYearSemester = new
                 CurrentYearSemesterOfArrangementEntity(yearSemester.getYear(), yearSemester.getSemester(), true);
         currentYearSemesterOfArrangementRepository.save(newYearSemester);
